732901bfb30b4578dd21d182c5bd06592c3edc11
GDieser <germandieser@gmail.com>
2025-05-11 23:33:49 -0300

    Agrega spawn de zombies

--- Assets/Scripts/SpawnZombies.cs
+++ Assets/Scripts/SpawnZombies.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SpawnZombies : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    [SerializeField] private GameObject zombie;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform Spawn1;
+
+    [SerializeField] private int total = 1;
+    private System.Random random = new System.Random();
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        while (total > 0)
+        {
+            SpawnZombie();
+            total--;
+        }
     }
+
+    private void SpawnZombie()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            int x = random.Next(-25, 25);
+            int y = random.Next(-16, 10);
+
+            GameObject newZombie = Instantiate(zombie, new Vector2(x, y), Spawn1.rotation);
+
+            ZombieScript zombieSc = newZombie.GetComponent<ZombieScript>();
+            zombieSc.player = player;
+
+        }
+
+    }
+
 }
